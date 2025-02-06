7efca24084fbd1e24ca1ccf8c503336a2a40a841
Jimmy Vega <ikarilink@gmail.com>
2025-02-05 20:16:24 -0600

    Some changes to the 1:30 hr tutorial checkpoint

--- src/components/navbar/Links/Links.jsx
+++ src/components/navbar/Links/Links.jsx
@@ -1,32 +1,61 @@
-import Link from "next/link";
+//import Link from "next/link";
+"use client";
+import { useState } from "react";
+import styles from "./links.module.css"
+import NavLink from "./navLink/navLink";
+
+const links = [
+   {
+      title: "Homepage",
+      path: "/"
+   },
+   {
+      title: "About",
+      path: "/about"
+   },
+   {
+      title: "Contact",
+      path: "/contact"
+   },
+   {
+      title: "Blog",
+      path: "/blog"
+   }
+];
 
 const Links = () =>  {
-   const links = [
-      {
-         title: "Homepage",
-         path: "/"
-      },
-      {
-         title: "About",
-         path: "/about"
-      },
-      {
-         title: "Contact",
-         path: "/contact"
-      },
-      {
-         title: "Blog",
-         path: "/blog"
-      }
-   ];
+   const [open,setOpen] = useState(false);
+
+   const session = true;
+   const isAdmin = true;
 
    return (
-      <div>
-         {links.map((link=>(
-            <Link href={link.path} key={link.title}>{link.title}</Link>
-         )))}
+      <div className={styles.container}>
+         <div className={styles.links}>
+            {links.map((link=>(
+               <NavLink item={link} key={link.title} />
+            )))}{
+               session ? (
+                  <>
+                     {isAdmin && <NavLink item = {{title: "Admin", path: "/admin"}} />}
+                     <button className={styles.logout}>Logout</button>
+                  </>
+               ) : (
+                  <NavLink item = {{title: "Login", path: "/login"}}/>
+               )
+            }
+         </div>
+         <button>Menu</button>
+         {
+            open && (
+            <div className={styles.mobileLinks}>
+               {links.map((link) => (
+                  <NavLink item={link} key={link.title}/>
+               ))}
+            </div>
+         )}
       </div>
-   )
+   );
 };
 
 export default Links;
